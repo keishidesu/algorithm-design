dbb812cdc8f23f9c16bb0676e9f0c8e4cb7fd4fc
manyimy <wong.manyi19@gmail.com>
2020-12-21 14:58:33 +0800

    Search sets for all sets

--- src/generator.cpp
+++ src/generator.cpp
@@ -15,6 +15,14 @@ Generator::Generator() {
   string* setB = new string[100000];
   string* setC = new string[500000];
 
+  string* newSetA = new string[10];
+  string* newSetB = new string[10];
+  string* newSetC = new string[10];
+
+  string* oldSetA = new string[10];
+  string* oldSetB = new string[10];
+  string* oldSetC = new string[10];
+
   //define a constant random seed
   srand(0);
   for(int i = 0; i < 100; i++)
@@ -31,6 +39,38 @@ Generator::Generator() {
     setC[i] = getRandomEmail();
 
   printArray(setC, 500000, "SetC");
+
+  // 10 items that cannot be found in each set
+  for(int i = 0; i < 10; i++)
+    newSetA[i] = getNewEmail(setA, newSetA);
+
+  printArray(newSetA, 10, "NewSetA");
+
+  for(int i = 0; i < 10; i++)
+    newSetB[i] = getNewEmail(setB, newSetB);
+
+  printArray(newSetB, 10, "NewSetB");
+
+  for(int i = 0; i < 10; i++)
+    newSetC[i] = getNewEmail(setC, newSetC);
+
+  printArray(newSetC, 10, "NewSetC");
+
+  // 10 items that can be found in each set
+  for(int i = 0; i < 10; i++)
+    oldSetA[i] = getOldEmail(setA, oldSetA);
+
+  printArray(oldSetA, 10, "OldSetA");
+
+  for(int i = 0; i < 10; i++)
+    oldSetB[i] = getOldEmail(setB, oldSetB);
+
+  printArray(oldSetB, 10, "OldSetB");
+
+  for(int i = 0; i < 10; i++)
+    oldSetC[i] = getOldEmail(setC, oldSetC);
+
+  printArray(oldSetC, 10, "OldSetC");
 }
 
 string Generator::getRandomName (int length){
@@ -60,6 +100,47 @@ string Generator::getRandomDomain (int length) {
 
 string Generator::getRandomEmail () {
   return getRandomName(4) + "." + getRandomName(5) + "@" + getRandomDomain(4);
+}
+
+string Generator::getNewEmail (string set[], string newset[]) {
+  bool existed;
+  string newEmail;
+
+  do {
+    existed = false;
+    newEmail = getRandomEmail();
+    for(int i = 0; i < sizeof(*set); i++) {
+      if(set[i] == newEmail) {
+        existed = true;
+        break;
+      }
+    }
+    for(int i = 0; i < sizeof(*newset); i++) {
+      if(newset[i] == newEmail) {
+        existed = true;
+        break;
+      }
+    }
+  } while(existed);
+  return newEmail;
+}
+
+string Generator::getOldEmail (string set[], string oldset[]) {
+  bool existed;
+  string oldEmail;
+
+  do {
+    existed = false;
+    oldEmail = set[rand() % sizeof(*set)];
+    for (int i = 0; i < sizeof(*oldset); i++) {
+      if(oldset[i] == oldEmail) {
+        existed = true;
+        break;
+      }
+    }
+  } while(existed);
+
+  return oldEmail;
 }
 
 template <typename T>
