a58a9d738c74c97357a5f42cf95527fa3292eee9
Shi Jing <shijingkoh@outlook.com>
2021-01-08 10:46:05 +0800

    modify comparison

--- src/HashTableLinear.cpp
+++ src/HashTableLinear.cpp
@@ -32,7 +32,6 @@ void HashTableLinear::insert(std::string email) {
   ComplexKey emailKey(email);
   int index = hashFunction(email);
   while (hashTable[index] != nullptr && hashTable[index]->getKey() != emailKey) {
-    std::cout << tableSize;
     if (++index >= tableSize) { 
       index = 0;
     }
